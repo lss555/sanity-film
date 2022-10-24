608f734f502c1d1179e0d33806d421e8c835473d
lss555 <chrisheibel7@gmail.com>
2022-10-23 18:45:19 -0600

    One film built out and responsive

--- sanity-film/src/components/one-film/one-film-styles.js
+++ sanity-film/src/components/one-film/one-film-styles.js
@@ -1,6 +1,65 @@
 import styled from 'styled-components';
+import ReactPlayer from 'react-player';
 
 export const OneFilmContainer = styled.div`
+	display: flex;
+	min-height: 100vh;
+	color: white;
+	padding-bottom: 3rem;
+
+	@media screen and (max-width: 1200px) {
+    flex-direction: column;
+    width: 100%;
+  }
+`;
+
+export const FilmTitle = styled.h1`
+	font-size: 40px;
+`;
+
+export const DirectedByText = styled.h3`
+	font-size: 24px;
+`;
+
+export const DescriptionText = styled.div`
+	font-size: 18px;
+`;
+
+export const DatePublished = styled.p`
+
+`;
+
+export const VideoContainer = styled.div`
+	display: flex;
 	margin: auto;
-	z-index: 50;
+	width: 50%;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	align-self: center;
+	@media screen and (max-width: 1200px) {
+		margin-top: -5rem;
+		padding-top: 0;
+		width: 85vw;
+
+  }
+`;
+
+export const ReactVideoPlayer =  styled(ReactPlayer)`
+	display: flex;
+	@media screen and (max-width: 1200px) {
+		
+  }
+`;
+
+export const ProjectTextContainer = styled.div`
+	width: 50%;
+	margin: auto 2rem auto 2rem;
+	text-align: center;
+	@media screen and (max-width: 1200px) {
+		width: 90%;
+		padding-right: .5rem;
+		padding-left: .5rem;
+		margin: auto;
+  }
 `;
--- sanity-film/src/components/one-film/one-film.js
+++ sanity-film/src/components/one-film/one-film.js
@@ -1,11 +1,65 @@
-import React from 'react';
-import { OneFilmContainer } from './one-film-styles.js';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../../client.js";
+import BlockContent from "@sanity/block-content-to-react";
+import { OneFilmContainer, FilmTitle, DirectedByText, DescriptionText, DatePublished, VideoContainer, ProjectTextContainer, ReactVideoPlayer } from './one-film-styles.js';
+
+
+
 
 const OneFilm = () => {
+  const [postData, setPostData] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == $slug]{
+          title,
+          slug,
+          url,
+          publishedAt,
+          mainImage{
+            asset->{
+              _id,
+              url
+             }
+           },
+         body,
+        "name": director->name,
+        "authorImage": author->image
+       }`,
+        { slug }
+      )
+      .then((data) => setPostData(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+ if (!postData) return <div>Loading...</div>;
 
 	return (
 		<OneFilmContainer>
-			<h1>one film</h1> 
+			<ProjectTextContainer>
+				<FilmTitle>{postData.title}</FilmTitle> 
+				<DirectedByText>Directed By: {postData.name}</DirectedByText>
+				
+				<DescriptionText>
+			        <BlockContent
+			          blocks={postData.body}
+			          projectId={sanityClient.clientConfig.projectId}
+			          dataset={sanityClient.clientConfig.dataset}
+			        />
+	      		</DescriptionText>
+	      		<DatePublished>{postData.publishedAt}</DatePublished>
+      		</ProjectTextContainer>
+      		<VideoContainer>
+				<ReactVideoPlayer 
+				controls
+				url={postData.url}
+				allow="true"
+      			allowFullScreen
+				/>
+			</VideoContainer>
 		</OneFilmContainer>
 		)
 };
